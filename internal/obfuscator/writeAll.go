d10b9961d51f3dcd910282974d7612ec12710ab5
zefixed <gerasenkov.code@mail.ru>
2026-03-28 19:33:12 +0300

    Fixed non-deterministic obfuscation order

--- internal/obfuscator/writeAll.go
+++ internal/obfuscator/writeAll.go
@@ -3,19 +3,35 @@ package obfuscator
 import (
 	"bytes"
 	"fmt"
+	"go/ast"
 	"go/format"
+	"golang.org/x/tools/go/packages"
 	"log/slog"
 	"os"
+	"sort"
 )
 
 func (o *Obfuscator) WriteAll() error {
 	o.log.Info(o.cfg.CurLocale["obf.info.writing.files"])
-	for _, pkg := range o.pkgs {
+
+	sortedPkgs := make([]*packages.Package, len(o.pkgs))
+	copy(sortedPkgs, o.pkgs)
+	sort.Slice(sortedPkgs, func(a, b int) bool {
+		return sortedPkgs[a].PkgPath < sortedPkgs[b].PkgPath
+	})
+
+	for _, pkg := range sortedPkgs {
 		if pkg.TypesInfo == nil {
 			continue
 		}
 
-		for _, file := range pkg.Syntax {
+		sortedFiles := make([]*ast.File, len(pkg.Syntax))
+		copy(sortedFiles, pkg.Syntax)
+		sort.Slice(sortedFiles, func(a, b int) bool {
+			return pkg.Fset.File(sortedFiles[a].Pos()).Name() < pkg.Fset.File(sortedFiles[b].Pos()).Name()
+		})
+
+		for _, file := range sortedFiles {
 			filePath := pkg.Fset.File(file.Pos()).Name()
 			o.log.Debug(o.cfg.CurLocale["obf.debug.writing.file"], slog.String("filename", filePath))
 
